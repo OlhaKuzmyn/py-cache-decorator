4d21d5082639c870d9ce57c4b937705c92ff6de6
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-02-27 17:53:11 +0200

    removed kwargs

--- app/main.py
+++ app/main.py
@@ -4,12 +4,12 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     stored_res = {}
 
-    def wrapper(*args, **kwargs) -> Callable:
+    def wrapper(*args) -> Callable:
         if args in stored_res:
             print("Getting from cache")
             return stored_res[args]
         print("Calculating new result")
-        result = func(*args, **kwargs)
+        result = func(*args)
         stored_res[args] = result
         return result
     return wrapper
